2bc33a0d1526a4d6faf8f0a403f9ec7fd87ee13e
ieee802 <hallo123box@web.de>
2018-06-12 02:55:19 +0200

    s

--- swa.js
+++ swa.js
@@ -46,9 +46,9 @@ self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-  const title = 'Push Codelab';
+  const title = 'Test';
   const options = {
-    body: 'Sende jetzt deine Email :)',
+    body: 'Test',
     icon: 'images/icon.png',
     badge: 'images/badge.png'
   };
